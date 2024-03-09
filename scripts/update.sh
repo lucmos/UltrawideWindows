dbe60c8735fe499bba301327bb78a2de84cbc6eb
Vangelis Tasoulas <vangelis@tasoulas.net>
2024-03-09 14:58:42 +0100

    "Perfected" the update/install/uninstall scripts
    We don't need to restart x11/wayland anymore when
    installing/uninstalling ultrawidewindows with the provided scripts.
    All the changes (and registered/unregistered shortcuts) are reflected
    immediately. This provides a much better user (but also development)
    experience.

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,9 +10,18 @@ cd "${cwd}"
 "$(dirname "${0}")"/release.sh
 
 kpackagetool6 --type=KWin/Script --install "${package_archive}"
-
-if [ "$(pgrep --count --full --list-full kwin_wayland)" -gt 0 ]; then
-  kwin_wayland --replace &
-else
-  kwin_x11 --replace &
+kwriteconfig6 --file kwinrc --group Plugins --key "${package_name}Enabled" true
+dbus-send --type=signal --dest=org.kde.KWin /KWin org.kde.KWin.reloadConfig
+# Wait until the config is reloaded, otherwise the loadScript
+# command below doesn't work and the script doesn't activate.
+# Seems like 1 sec sleep is enough
+sleep 1
+loaded=$(qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript "${package_name}")
+if [ "${loaded}" -ne 1 ]; then
+  echo >&2 "Failed to load ${package_name}. Try to logout and login again"
+  exit 1
 fi
+
+# Reload the configuration and make the shortcuts appear in the kglobalshortcutsrc
+# without having to logout and log back in
+qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.cleanUp > /dev/null
